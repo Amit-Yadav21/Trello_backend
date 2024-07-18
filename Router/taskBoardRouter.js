14e0e5ba28b6ce3c1ce23da6ae5542fdc03dbe78
Amit-Yadav21 <amit21navgurukul.org>
2024-07-19 00:31:31 +0530

    give right path task Board controller in router

--- Router/taskBoardRouter.js
+++ Router/taskBoardRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { findTasksByStatus } from '../Controller/taskController.js';
+import { findTasksByStatus } from '../Controller/taskBoardController.js';
 
 const router = express.Router();
 router.get('/taskBoard/find/task/By/Status', findTasksByStatus);
